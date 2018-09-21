e9d1ba3d11018f0d073ee070b466da3ad48427e8
Marshall Offutt <marshalloffutt@gmail.com>
2018-09-21 17:13:48 -0500

    Adds bootstrap form

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('i am linked');
+const printToDom = (stringToPrint, selectedDiv) => {
+    document.getElementById(selectedDiv).innerHTML += stringToPrint;
+};
+
+sortFormCard = () => {
+   document.getElementById('form').classList.replace('d-none', 'd-block')
+};
+
+document.getElementById('formButton').addEventListener("click", sortFormCard);
